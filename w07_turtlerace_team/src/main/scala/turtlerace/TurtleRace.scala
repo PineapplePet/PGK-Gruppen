da60d2a7d530ebb5ef3666851beabc3f7976fdf5
Niklas <ni3352be-s@student.lu.se>
2016-10-12 14:56:08 +0200

    turtlerace

--- w07_turtlerace_team/src/main/scala/turtlerace/TurtleRace.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/TurtleRace.scala
@@ -3,16 +3,16 @@ package turtlerace;
 import scala.collection.mutable.ArrayBuffer
 import cslib.window._
 
-object TurtleRace {
+object TurtleRace extends RaceTurtle{
   /**
     * Perform a race between eight turtles and returns the turtles in finishing order
     */
   def race(turtles: Seq[RaceTurtle], rw: RaceWindow, title: String): List[RaceTurtle] = {
-    RaceTurtle.printRacers
-    var winners = ArrayBuffer.empty[]
-    if () {
-      RaceTurtle.step
-
+    rw.printRacers
+    var winners = ArrayBuffer.empty[RaceTurtle]
+    while (position.x < 750) {
+      raceStep()
     }
+    
   }
 }
