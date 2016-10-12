0ff10c6c4e44941c9faa91e5e3bf24f3b0570498
Trolliver <trolliver1337@hotmail.se>
2016-10-12 11:51:17 +0200

    RaceWindow Added Image

--- w07_turtlerace_team/src/main/scala/turtlerace/RaceWindow.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/RaceWindow.scala
@@ -1,10 +1,16 @@
 package turtlerace;
 import cslib.window._
 
+import java.io.File
+import javax.imageio.ImageIO
+
 class RaceWindow extends SimpleWindow(800,400,"Race"){
 
-  private val startX = ???
-  private val endX = ???
+  private val startX = 50
+  private val endX = 750
+
+
+  val seelpic = ImageIO.read(new File("/h/d7/s/ol4084da-s/ProgZip/workspace/PGK-Gruppen/w07_turtlerace_team/src/main/scala/turtlerace/racebana.png"))
   /**
    * Draws a race in the RaceWindow
    */
@@ -13,15 +19,15 @@ class RaceWindow extends SimpleWindow(800,400,"Race"){
   /**
    * Returns the Y-coordinate for the turtle with start number n
    */
-  def getStartY(n: Int): Int = ???
+  def getStartY(n: Int): Int = { n*40 + 10}
   
   /**
    * Returns the X-coordinate of the starting position
    */
-  def getStartX: Int = ???
+  def getStartX: Int = { startX}
   
   /**
    * Returns the X-coordinate of the finish line
    */
-  def getEndX: Int = ???
+  def getEndX: Int = { endX}
 }
