c2bb0575215bd6e4ed06cb9a140a5fcafe0b01f1
Daniel-Huber <daniel.huber@cmail.nu>
2016-10-12 15:05:30 +0200

    TurtleRace

--- w07_turtlerace_team/src/main/scala/turtlerace/TurtleRace.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/TurtleRace.scala
@@ -8,7 +8,7 @@ object TurtleRace extends RaceTurtle{
     * Perform a race between eight turtles and returns the turtles in finishing order
     */
   def race(turtles: Seq[RaceTurtle], rw: RaceWindow, title: String): List[RaceTurtle] = {
-    rw.printRacers(turtles, rw.getStartX, )
+    rw.printRacers(turtles, rw.getStartX, title)
     rw.printTitle(title)
     var winners = ArrayBuffer.empty[RaceTurtle]
     while (position.x < 750) {
