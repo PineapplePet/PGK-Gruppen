f9f5e425be8836b1235033956a015208a638fe4f
AlexR <tillalex@gmail.com>
2016-10-12 12:21:20 +0200

    kommentar i simplewindow

--- w07_turtlerace_team/src/main/scala/turtlerace/FileUtils.scala
+++
@@ -1,9 +0,0 @@
-package turtlerace
-
-object FileUtils {
-
-  import java.nio.file.{Paths, Files}
-
-  def save(s: String, fileName: String): Unit =
-    Files.write(Paths.get(fileName), s.getBytes("UTF-8"))
-}
--- w07_turtlerace_team/src/main/scala/turtlerace/Main.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/Main.scala
@@ -1,11 +1,10 @@
 package turtlerace
-import w06_turtlegraphics.turtlegraphics_
+
 import cslib.window.SimpleWindow
 
 object Main {
   def main(args : Array[String]): Unit = {
-    FileUtils.save("text2", "apelsin.txt")
-    println("tja")
+
     val w = new RaceWindow
     w.draw
   }
