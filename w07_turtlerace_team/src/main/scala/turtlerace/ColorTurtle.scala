8438cc24c6ea6e81d21ad92b9c393f65432aad0f
AlexR <tillalex@gmail.com>
2016-10-13 09:09:29 +0200

    kommentar i simplewindow

--- w07_turtlerace_team/src/main/scala/turtlerace/ColorTurtle.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/ColorTurtle.scala
@@ -5,7 +5,7 @@ import cslib.window.SimpleWindow
 class ColorTurtle(window: SimpleWindow,
                   position: Point = Point(0, 0),
                   angle: Double = 0,
-                  isPenDown: Boolean = true, //ändra till false om man vill ha groda
+                  isPenDown: Boolean = false,
                   private var color: java.awt.Color) extends Turtle(window, position, angle, isPenDown) {
 
   override def forward(length: Double): Unit = {
