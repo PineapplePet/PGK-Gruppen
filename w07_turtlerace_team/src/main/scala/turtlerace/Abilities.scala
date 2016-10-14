1bbd3cabe523e056b8c4db6d38fd4e2ab9981301
Niklas <ni3352be-s@student.lu.se>
2016-10-14 14:52:04 +0200

    ny trait

--- w07_turtlerace_team/src/main/scala/turtlerace/Abilities.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/Abilities.scala
@@ -69,7 +69,7 @@ trait KeD extends RaceTurtle { //Går dubbelt så långt eller inget
 trait hax extends RaceTurtle { //fuskar och hoppar direkt till slutet (låg chans)
   val stepR = new Random()
   override def raceStep() = {
-    //hoppar till mål (<1% chans) annars går 0.5 i x-led
+    //hoppar till mål (<0.1% chans) annars går 0.5 i x-led
     if (math.random < 0.001) {
       position = Point(750, position.y)
     }
