c271f90b93f532f41313bbde3ae8e08c30d74213
Trolliver <trolliver1337@hotmail.se>
2016-10-14 15:05:06 +0200

    semifinaler med fel nummer maybe

--- w07_turtlerace_team/src/main/scala/turtlerace/Abilities.scala
+++ w07_turtlerace_team/src/main/scala/turtlerace/Abilities.scala
@@ -68,14 +68,16 @@ trait KeD extends RaceTurtle { //Går dubbelt så långt eller inget
 
 trait hax extends RaceTurtle { //fuskar och hoppar direkt till slutet (låg chans)
   val stepR = new Random()
-  override def raceStep() = { //hoppar till mål (<1% chans) annars går 0.5 i x-led
-    if (math.random < 0.01)
+  override def raceStep() = {
+    //hoppar till mål (<1% chans) annars går 0.5 i x-led
+    if (math.random < 0.01) {
       position = Point(450, position.y)
     }
-    else if (math.random >= 0.01)  {
-       forward(0.5)
-    }
 
+    else if (math.random >= 0.01) {
+      forward(0.5)
+    }
+  }
   override def toString: String = super.toString + " hax"
 }
 
