7fe35f948157ea5e0f8242c0935f5590a92218c5
AlexR <tillalex@gmail.com>
2016-11-30 15:04:06 +0100

    a

--- w11_lthopoly_team/src/main/java/lthopoly/parser/DocumentParser.java
+++ w11_lthopoly_team/src/main/java/lthopoly/parser/DocumentParser.java
@@ -5,9 +5,9 @@ import lthopoly.cards.MoveCard;
 import lthopoly.spaces.BoardSpace;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 /**
  * Created by Tank on 4/17/2016.
  */
@@ -36,19 +36,23 @@ public class DocumentParser {
      */
     public static MoveCard[] getMoveCards() {
 
-        Scanner scan = new Scanner(f);
+         Scanner scan = null;
+        ArrayList<MoveCard> array = new ArrayList<MoveCard>();
+        int i = 0;
 
-
-        for (int i = 0; i < f.getLines; i++){
-
+        try {
+            scan = new Scanner(new File("movecards.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("EXCEPTION: " + e);
         }
 
+        while (scan.hasNext()) {                                                                   //kollar om det finns en till rad
+            array.add(i, new MoveCard(scan.nextLine().split(";")[0], Integer.parseInt(scan.nextLine().split(";")[1]))); //lägger till nytt MoveCard i array
+            i++;
+        }
+        return array.toArray(new MoveCard[]{});
 
-
-        return null;
     }
-        // Använda detta? ->
-    //
 
 
 }
